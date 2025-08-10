0069a86a79a0744c1afda91987cdea937147e8c8
Sai Balkawade <saibalkawade10@gmail.com>
2025-08-10 13:35:57 +0530

    ff

--- backend/frontend/postcss.config.js
+++ backend/frontend/postcss.config.js
@@ -1,10 +1,7 @@
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
+import autoprefixer from "autoprefixer";
 
 export default {
   plugins: {
-    '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
-}
-
+};
